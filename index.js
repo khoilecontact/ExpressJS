e6c473f336b5901e4fd9579143eb999b0a7b43ff
khoilecontact <67513142+khoilecontact@users.noreply.github.com>
2021-03-19 15:59:37 +0700

    7rd lesson - params and dynamic routing

--- index.js
+++ index.js
@@ -3,18 +3,27 @@ var express = require('express')
 const app = express() // tao 1 express moi
 const port = 3000
 var bodyParser = require('body-parser')
+const shortid = require('shortid')
+const low = require('lowdb') // require lowdb to use 
+const FileSync = require('lowdb/adapters/FileSync') // select adapter 
+var adpter = new FileSync('db.json')
+const db = low(adpter) // create a new object by calling low and input adapter
+
+//set some defaults 
+db.defaults({ users: [] })
+  .write();
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 
-var users = [
+/* var users = [
   {id: 1, name: "Khoi Le"},
   {id: 2, name: "Khoi Nguyen"},
   {id: 3, name: "Thinh Tran"}
 
 
-]; // array of objects
+]; // array of objects */
 
 app.set('view engine', 'pug'); //install pug as view
 app.set('views', './views'); // view 
@@ -28,26 +37,50 @@ app.get('/',(req,res)=> {
 
 app.get('/users',(req,res)=> {
   res.render('users/index.pug', {
-    users: users //truyen vao mang objects
+    users: db.get('users').value() //truyen vao mang objects tu database
   })
 }) 
 
+/* app.get('/users/search', (req,res) => {
+  var q=req.query.q;
+  var matchedUsers = db.get('users').value().filter(function(user) {
+    return (db.get('users').value().toLowerCase().indexOf(q.toLowerCase()) != -1);
+  }); //filter cac user co cac chu cai tim kiem trong user.name
+  res.render('users/index', {
+    users: matchedUsers
+  })
+} ) //testing not official */ 
+
 app.get('/users/search', (req,res) => {
   var q=req.query.q;
-  var matchedUsers = users.filter(function(user) {
-    return (user.name.toLowerCase().indexOf(q.toLowerCase()) != -1);
+  var arrayUsers = db.get('users').value();
+  var matchedUsers = arrayUsers.filter(function(user) {
+    return (user.toLowerCase().indexOf(q.toLowerCase()) != -1);
   }); //filter cac user co cac chu cai tim kiem trong user.name
   res.render('users/index', {
     users: matchedUsers
   })
 } )
 
+
 app.get('/users/create', (req, res)=> {
 res.render('users/create')
 }); //tao site create va tro den template create trong thu muc users 
 
+app.get('/users/:id', (req,res)=>{ //dynamic routing :id
+  var id = req.params.id //add contained variable 
+  var user = db.get('users').find({id: id}).value()
+
+  res.render('users/view', {
+    user: user
+  })
+})
+
 app.post('/users/create', (req, res)=> {
-users.push(req.body); // save information posted by human
+req.body.id=shortid.generate();
+db.get('users').push(req.body).write(); // save information posted by human 
+// replace users by db.get('users') cuz we are using database now 
+// .write() to save data to file  
 res.redirect('/users');
 });
 
